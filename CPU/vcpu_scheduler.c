0f24af524b9631d2e957333bb11d47ea747a8f35
Bond Wong <bond@Bonds-MacBook-Pro.local>
2019-09-05 22:07:41 -0700

    .

--- CPU/vcpu_scheduler.c
+++ CPU/vcpu_scheduler.c
@@ -11,21 +11,21 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  // get all active running virtual machines
-  // int domainCnt = virConnectNumOfDomains(conn);
-  // int *activeDomains = malloc(sizeof(int) * domainCnt);
-  // int res = virConnectListDomains(conn, domainCnt, activeDomains);
+  get all active running virtual machines
+  int domainCnt = virConnectNumOfDomains(conn);
+  int *activeDomains = malloc(sizeof(int) * domainCnt);
+  int res = virConnectListDomains(conn, domainCnt, activeDomains);
 
-  // launch VM and see how vCUP is associated with pCUP
-  // virDomainInfoPtr domainInfo = malloc(sizeof(virDomainInfo));
-  // if (virDomainGetInfo(activeDomains[0], domainInfo) != 0) {
-  //   fprintf(stderr, "Failed to get domain info");
-  //   exit(1);
-  // }
+  launch VM and see how vCUP is associated with pCUP
+  virDomainInfoPtr domainInfo = malloc(sizeof(virDomainInfo));
+  if (virDomainGetInfo(activeDomains[0], domainInfo) != 0) {
+    fprintf(stderr, "Failed to get domain info");
+    exit(1);
+  }
 
-  // virVcpuInfoPtr vcupInfo =malloc(sizeof(virVcpuInfo) * domainInfo->nrVirtCpu);
-  // virDomainGetVcpus(activeDomains[0], vcupInfo, domainInfo->nrVirtCpu, NULL, 0);
+  virVcpuInfoPtr vcupInfo =malloc(sizeof(virVcpuInfo) * domainInfo->nrVirtCpu);
+  virDomainGetVcpus(activeDomains[0], vcupInfo, domainInfo->nrVirtCpu, NULL, 0);
 
-  // virConnectClose(conn);
+  virConnectClose(conn);
   return 0;
 }
