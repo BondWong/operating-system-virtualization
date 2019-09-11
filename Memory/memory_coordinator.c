668bce870fa6c13630ad8f216638f15736cdc9b1
Bond Wong <bond@Bonds-MacBook-Pro.local>
2019-09-10 23:18:38 -0700

    .

--- Memory/memory_coordinator.c
+++ Memory/memory_coordinator.c
@@ -93,12 +93,12 @@ int main(int argc, char *argv[]) {
     // sort memstat of each VMs in desneding order
     fprintf(stdout, "%s\n", "Getting domain memeory stat");
     MemStatPtr memStats = malloc(sizeof(struct MemStat) * domainCnt);
-    getAndSortMemStat(conn, memStats, activeDomains, domainCnt);
+    getAndSortMemStat(conn, memStats, activeDomains, domainCnt, interval);
     // iterate each, collect memory from those have wasteful memory, assign to those are starving
     // during the iteration, if memory remain is negative, start using hypervisor memory
     // in the end, if remaining is positive, assign back to hypervisor
     fprintf(stdout, "%s\n", "Rebalancing domain memeory");
-    unsigned long long freeMemory = hostFreeMemory(conn) / 1024;
+    unsigned long long freeMemory = virNodeGetFreeMemory(conn) / 1024;
     rebalanceMemory(memStats, activeDomains, domainCnt, freeMemory);
     free(memStats);
     sleep(interval);
