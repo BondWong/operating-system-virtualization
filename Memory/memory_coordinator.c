f0e0a234d4291fe4d2ea40674c24ffad17f6e0bd
Bond Wong <bond@Bonds-MacBook-Pro.local>
2019-09-12 22:01:37 -0700

    .

--- Memory/memory_coordinator.c
+++ Memory/memory_coordinator.c
@@ -4,13 +4,13 @@
 #include <unistd.h>
 #include <string.h>
 
-const unsigned long ABUNDANCE_THRESHOLD = 100 * 1024; // 500 MB
+const unsigned long ABUNDANCE_THRESHOLD = 0.6;
+const unsigned long HOST_MINIMUM = 200 * 1024;
 
 struct MemStat {
   virDomainPtr domain;
   virDomainInfoPtr domainInfo;
   unsigned long memory;
-  unsigned long availableMemory;
 };
 
 typedef struct MemStat* MemStatPtr;
@@ -32,24 +32,22 @@ void getAndSortMemStat(virConnectPtr conn, MemStatPtr memStats, const int* activ
     virDomainMemoryStats(domain, memStatStruct, VIR_DOMAIN_MEMORY_STAT_NR, 0);
     memStats[i].domain = domain;
     memStats[i].domainInfo = domainInfo;
-    memStats[i].memory = memStatStruct[VIR_DOMAIN_MEMORY_STAT_UNUSED].val;
-    memStats[i].availableMemory = memStatStruct[VIR_DOMAIN_MEMORY_STAT_AVAILABLE].val;
+    memStats[i].memory = memStatStruct[VIR_DOMAIN_MEMORY_STAT_AVAILABLE].val;
   }
 
   qsort((void *)memStats, domainCnt, sizeof(struct MemStat), comparator);
   for (int i = 0; i < domainCnt; i++) {
-    fprintf(stdout, "domain %s -- unused memory %lu -- available memory %lu -- used memory %lu -- total memory %lu\n",
-      virDomainGetName(memStats[i].domain), memStats[i].memory, memStats[i].availableMemory, memStats[i].domainInfo->memory, memStats[i].domainInfo->maxMem);
+    fprintf(stdout, "domain %s -- available memory %lu / total memory %lu\n",
+      virDomainGetName(memStats[i].domain), memStats[i].memory, memStats[i].domainInfo->maxMem);
   }
 }
 
   void rebalanceMemory(MemStatPtr memStats, int *activeDomains, int domainCnt, unsigned long long freeMemory) {
     unsigned long remain = 0;
+    unsigned long threshold = ABUNDANCE_THRESHOLD * memStats[i].domainInfo->maxMem;
     for (int i = 0; i < domainCnt; i++) {
-      if (memStats[i].memory == ABUNDANCE_THRESHOLD) {
-        continue;
-      } else if (memStats[i].memory > ABUNDANCE_THRESHOLD) {
-        unsigned long reclaim = memStats[i].memory - ABUNDANCE_THRESHOLD;
+      if (memStats[i].memory > threshold) {
+        unsigned long reclaim = memStats[i].memory - threshold;
         remain += reclaim;
         // hypervisor inflats balloon to reclaim memory
         unsigned long newMemorySize = memStats[i].domainInfo->maxMem - reclaim;
@@ -57,10 +55,10 @@ void getAndSortMemStat(virConnectPtr conn, MemStatPtr memStats, const int* activ
         virDomainSetMaxMemory(memStats[i].domain, newMemorySize);
         fprintf(stdout, "New memory size is %lu\n", newMemorySize);
       } else {
-        unsigned long assign = ABUNDANCE_THRESHOLD - memStats[i].memory;
+        unsigned long assign = threshold - memStats[i].memory;
         remain -= assign;
         // if hypervisor itself is starving, don't assign
-        if (remain <= 0 && freeMemory <= ABUNDANCE_THRESHOLD) {
+        if (remain <= 0 && freeMemory <= HOST_MINIMUM) {
           fprintf(stdout, "Insufficient memory in the hypervisor skipping \n");
           break;
         }
