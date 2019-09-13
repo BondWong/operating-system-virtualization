6ec3dd4a4e2d4c2a4aaa900680cd9168e336bc16
Bond Wong <bond@Bonds-MacBook-Pro.local>
2019-09-12 22:36:45 -0700

    .

--- Memory/memory_coordinator.c
+++ Memory/memory_coordinator.c
@@ -53,7 +53,8 @@ void getAndSortMemStat(virConnectPtr conn, MemStatPtr memStats, const int* activ
         // hypervisor inflats balloon to reclaim memory
         unsigned long newMemorySize = memStats[i].memory - reclaim;
         fprintf(stdout, "Reclaiming memeory %lu from domain %s \n", reclaim, virDomainGetName(memStats[i].domain));
-        virDomainSetMemory(memStats[i].domain, newMemorySize);
+        int res = virDomainSetMemory(memStats[i].domain, newMemorySize);
+        if (res == -1) fprintf(stderr, "fail to set memory\n");
         fprintf(stdout, "New memory size is %lu\n", newMemorySize);
       } else {
         unsigned long assign = memStats[i].domainInfo->maxMem * MEMORY_CHANGE_RATE;
@@ -67,6 +68,7 @@ void getAndSortMemStat(virConnectPtr conn, MemStatPtr memStats, const int* activ
         unsigned long newMemorySize = memStats[i].memory + assign;
         fprintf(stdout, "Assigning memeory %lu to domain %s \n", assign, virDomainGetName(memStats[i].domain));
         virDomainSetMemory(memStats[i].domain, newMemorySize);
+        if (res == -1) fprintf(stderr, "fail to set memory\n");
         fprintf(stdout, "New memory size is %lu\n", newMemorySize);
       }
     }
